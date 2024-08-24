263ed19f4bd3a3c52c45e66be6b0cd10f15a9df7
OriBraverman <oribraverman@gmail.com>
2024-08-24 16:44:13 +0300

    minur change

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from matplotlib import pyplot as plt
 import numpy as np
 import torch
@@ -68,6 +69,59 @@ def generate_rbp_intensity_correlation_heatmap(sequences_file, intensities_dir, 
 
     print(f"Heatmap saved as '{output_file}'")
 
-def get_person_correlation(self, predictions, intensities):
+def get_person_correlation(predictions, intensities):
     """Calculates the Pearson correlation between the predictions and intensities."""
     return np.corrcoef(predictions, intensities)[0, 1]
+
+intensities_dir='data/RNAcompete_intensities'
+predict_output_dir='outputs/predictions/ImprovedDeepSELEX'
+
+def get_min_max_intensities(intensities_dir):
+    intensities = []
+    max_arr = []
+    min_arr = []
+    for i in range(1, 39):
+        intensities_file = os.path.join(intensities_dir, f'RBP{i}.txt')
+        if os.path.exists(intensities_file):
+            intensities.append(pd.read_csv(intensities_file, header=None).values.flatten())
+            max_arr.append(np.max(intensities[-1]))
+            min_arr.append(np.min(intensities[-1]))
+    with open('min_max_intensities.txt', 'w') as f:
+        for i in range(38):
+            f.write(f'RBP{i+1}: Min={min_arr[i]}, Max={max_arr[i]}\n')
+        f.write(f'Min: {min(min_arr)}\n')
+        f.write(f'Max: {max(max_arr)}\n')
+def get_intensity_bins(intensities_dir):
+    bins = []
+    ranges = []
+    for i in range(1, 39):
+        intensities_file = os.path.join(intensities_dir, f'RBP{i}.txt')
+        if os.path.exists(intensities_file):
+            intensities = pd.read_csv(intensities_file, header=None).values.flatten()
+            # Divide intensities into 5 classes
+            hist, bin_edges = np.histogram(intensities, bins=5)
+            # Calculate ranges and count items in each class
+            class_info = []
+            for i in range(5):
+                class_range = (bin_edges[i], bin_edges[i + 1])
+                count = hist[i]
+                class_info.append((class_range, count))
+            bins.append(hist)
+            ranges.append(class_info)
+    with open('intensity_bins.txt', 'w') as f:
+        for i in range(38):
+            f.write(f'RBP{i+1}: {bins[i]}\n')
+            # Calculate the percentage for each element in bins[i]
+            percentages = (bins[i] / np.sum(bins[i])) * 100
+            # Create a string that contains all percentages formatted to 2 decimal places
+            percentages_str = ', '.join([f'{p:.2f}%' for p in percentages])
+            # Write the formatted string to the file
+            f.write(f'RBP{i + 1}: {percentages_str}\n')
+            f.write(f'Ranges: {ranges[i]}\n')
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
